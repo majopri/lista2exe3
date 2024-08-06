f6d695fdb62dbbbf3f6231c44eb6f9271a058b60
User <User@DESKTOP-G0A3IIV>
2024-08-06 03:31:58 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2exe3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada de valores
+            Console.Write("digite o primeiro valor:");
+            int valor1 = int.Parse(Console.ReadLine());
+            
+
+            Console.Write("digite o segundo valor:");
+            int valor2 = int.Parse(Console.ReadLine());
+
+            Console.Write("digite o terceiro valor:");
+            int valor3 = int.Parse(Console.ReadLine());
+
+            Console.Write("valor1 = valor2 = valor3:");
+
+
+            Console.WriteLine("todos os numeros são identicos.");
+            
+            
+        }
+    }
+}
